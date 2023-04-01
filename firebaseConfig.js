d1128762083b9f4e81d064ff836651623f8fb2ac
Your name <dreyer.wil@gmail.com>
2023-04-01 12:44:10 +0200

    firebasego

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,14 +3,17 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBcemcL6XHoOZaefYWvsU7SMeA-uCczfMA",
-    authDomain: "aeshmanager.firebaseapp.com",
-    projectId: "aeshmanager",
-    storageBucket: "aeshmanager.appspot.com",
-    messagingSenderId: "655057665330",
-    appId: "1:655057665330:web:52c689822758a61cf8eb2b"
+const firebaseConfig = {  
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+
 };
+
+
 
 const app = initializeApp(firebaseConfig);
 
